030d7c033eb2743454b314550728bfdd6bc6e29b
nexus0208 <christiandelacruz0208@gmail.com>
2023-04-30 09:51:16 +0800

    add some layout

--- src/Home.jsx
+++ src/Home.jsx
@@ -4,7 +4,17 @@ export default class Home extends Component {
     render() {
         return (
             <div className="home-container">
-                Home
+                <div className="max-width home-grid">
+                    <div className="profile-photo"></div>
+                    <div className="profile-bibliography">
+                        <div className="home-about-me margin-15px">About me</div>
+                        <div className="home-bibliography margin-15px">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Accusamus voluptates inventore iste omnis modi, perferendis tempora assumenda, est dignissimos quo nobis at, unde culpa eius beatae eaque molestiae vitae perspiciatis.</div>
+                        <div className="home-links margin-15px">
+                            <button>Github</button>
+                            <button>Linked In</button>
+                        </div>
+                    </div>
+                </div>
             </div>
         );
     }

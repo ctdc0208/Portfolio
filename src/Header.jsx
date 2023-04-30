96525221639b786ab2c82cd699c675ad00fb143c
nexus0208 <christiandelacruz0208@gmail.com>
2023-04-30 09:51:05 +0800

    change button color css

--- src/Header.jsx
+++ src/Header.jsx
@@ -3,11 +3,11 @@ import { Component } from "react";
 export default class Header extends Component {
     render() {
         return (
-            <div className="max-width margin-auto padding-auto justify-end header-container">
-                <button>Home</button>
-                <button>About</button>
-                <button>Projects</button>
-                <button>Contact</button>
+            <div className="max-width padding-auto justify-end header-container">
+                <button className="button-header">Home</button>
+                <button className="button-header">About</button>
+                <button className="button-header">Projects</button>
+                <button className="button-header">Contact</button>
             </div>
         );
     }

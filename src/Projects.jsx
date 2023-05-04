dc9787816bb1f25a2aabfc158aca7642ee9ebfa7
nexus0208 <christiandelacruz0208@gmail.com>
2023-05-05 07:14:27 +0800

    add project cards

--- src/Projects.jsx
+++ src/Projects.jsx
@@ -3,8 +3,74 @@ import { Component } from "react";
 export default class Projects extends Component {
     render() {
         return (
-            <div className="max-width padding-auto projects-bg">
-                <div>My Work</div>
+            <div className="projects-bg">
+            <div className="max-width padding-auto">
+                <div className="projects-container">
+                    <div className="my-works">
+                        My Works
+                    </div>
+                    <div className="projects-card project-1">
+                        <div className="project-screenshot">Screenshot of Project</div>
+                        <div className="project-info margin-15px">
+                            <div className="project-header">
+                                <div className="project-name">Project One</div>
+                                <div className="project-links">
+                                    <button>View Code</button>
+                                     <button>Live Preview</button>
+                                </div>
+                            </div>
+                            <div className="project-description">
+                                Short description of the project. Just a couple sentences will do
+                            </div>
+                        </div>
+                    </div>
+                    <div className="projects-card project-2">
+                        <div className="project-screenshot">Screenshot of Project</div>
+                        <div className="project-info margin-15px">
+                            <div className="project-header">
+                                <div className="project-name">Project One</div>
+                                <div className="project-links">
+                                    <button>View Code</button>
+                                     <button>Live Preview</button>
+                                </div>
+                            </div>
+                            <div className="project-description">
+                                Short description of the project. Just a couple sentences will do
+                            </div>
+                        </div>
+                    </div>
+                    <div className="projects-card project-3">
+                        <div className="project-screenshot">Screenshot of Project</div>
+                        <div className="project-info margin-15px">
+                            <div className="project-header">
+                                <div className="project-name">Project One</div>
+                                <div className="project-links">
+                                    <button>View Code</button>
+                                     <button>Live Preview</button>
+                                </div>
+                            </div>
+                            <div className="project-description">
+                                Short description of the project. Just a couple sentences will do
+                            </div>
+                        </div>
+                    </div>
+                    <div className="projects-card project-4">
+                        <div className="project-screenshot">Screenshot of Project</div>
+                        <div className="project-info margin-15px">
+                            <div className="project-header">
+                                <div className="project-name">Project One</div>
+                                <div className="project-links">
+                                    <button>View Code</button>
+                                     <button>Live Preview</button>
+                                </div>
+                            </div>
+                            <div className="project-description">
+                                Short description of the project. Just a couple sentences will do
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
             </div>
         )
     }

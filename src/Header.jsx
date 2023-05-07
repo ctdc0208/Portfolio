2d406c0195fca6aee233ad60d46ee8080fb35754
nexus0208 <christiandelacruz0208@gmail.com>
2023-05-07 16:59:20 +0800

    transparent header with pseudo before

--- src/Header.jsx
+++ src/Header.jsx
@@ -3,11 +3,13 @@ import { Component } from "react";
 export default class Header extends Component {
     render() {
         return (
-            <div className="max-width padding-auto justify-end header-container">
-                <button className="button-header">Home</button>
-                <button className="button-header">About</button>
-                <button className="button-header">Projects</button>
-                <button className="button-header">Contact</button>
+            <div className="header-container">
+                <div className="max-width padding-auto justify-end">
+                    <button className="button-header">Home</button>
+                    <button className="button-header">About</button>
+                    <button className="button-header">Projects</button>
+                    <button className="button-header">Contact</button>
+                </div>
             </div>
         );
     }

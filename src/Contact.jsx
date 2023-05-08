be5152ba52dd032cef4985db7d54512cba817071
nexus0208 <christiandelacruz0208@gmail.com>
2023-05-08 13:57:11 +0800

    project container

---
+++ src/Contact.jsx
@@ -0,0 +1,15 @@
+import { Component } from "react";
+
+export default class Contact extends Component {
+    render() {
+        return (
+            <div className="background-black">
+            <div className="max-width">
+                <div className="contact-container">
+                    <div className="contact-me">Contact</div>
+                </div>
+            </div>
+            </div>
+        )
+    }
+}

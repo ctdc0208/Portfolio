18c5bf6897bc92b080643013e2bada6dabcb2539
nexus0208 <christiandelacruz0208@gmail.com>
2023-05-11 18:13:29 +0800

    add slideup animation

--- src/Home.jsx
+++ src/Home.jsx
@@ -6,8 +6,13 @@ export default class Home extends Component {
             <div className="home-container">
                 <div className="max-width">
                     <div className="home-name">
-                        <div className="first-name">Christian dela Cruz</div>
+                        <div className="overflow-hidden">
+                            <div className="name">
+                                Christian dela Cruz</div>
+                        </div>
+                        <div className="overflow-hidden">
                         <div className="web-developer">Web Developer</div>
+                        </div>
                 </div>
                     </div>
             </div>

de9ae2a6ebb696989df8d36aa77401a4a3c91f7a
nexus0208 <christiandelacruz0208@gmail.com>
2023-05-11 13:20:06 +0800

    add box shadow to project screenshots

--- src/Projects.jsx
+++ src/Projects.jsx
@@ -25,8 +25,8 @@ export default class Projects extends Component {
                         </div>
                     </div>
                     <div className="projects-card project-2">
-                        <div className="project-screenshot">Screenshot of Project</div>
-                        <div className="project-info margin-15px">
+                        <div className="project-screenshot-opposite">Screenshot of Project</div>
+                        <div className="project-info-opposite margin-15px">
                             <div className="project-header">
                                 <div className="project-name">Project Two</div>
                                 <div className="project-links">
@@ -44,21 +44,6 @@ export default class Projects extends Component {
                         <div className="project-info margin-15px">
                             <div className="project-header">
                                 <div className="project-name">Project Three</div>
-                                <div className="project-links">
-                                    <button>View Code</button>
-                                     <button>Live Preview</button>
-                                </div>
-                            </div>
-                            <div className="project-description">
-                                Short description of the project. Just a couple sentences will do
-                            </div>
-                        </div>
-                    </div>
-                    <div className="projects-card project-4">
-                        <div className="project-screenshot">Screenshot of Project</div>
-                        <div className="project-info margin-15px">
-                            <div className="project-header">
-                                <div className="project-name">Project Four</div>
                                 <div className="project-links">
                                     <button>View Code</button>
                                      <button>Live Preview</button>

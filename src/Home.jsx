5e3c3f95e312d93aee0c364a7ca832bd2819b1e9
nexus0208 <christiandelacruz0208@gmail.com>
2023-05-11 13:36:30 +0800

    add border-bottom

--- src/Home.jsx
+++ src/Home.jsx
@@ -4,10 +4,12 @@ export default class Home extends Component {
     render() {
         return (
             <div className="home-container">
-                <div className="max-width home-name">
-                    <div className="first-name">Christian dela Cruz</div>
-                    <div className="web-developer">Web Developer</div>
+                <div className="max-width">
+                    <div className="home-name">
+                        <div className="first-name">Christian dela Cruz</div>
+                        <div className="web-developer">Web Developer</div>
                 </div>
+                    </div>
             </div>
         );
     }

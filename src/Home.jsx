bbee3c63834251ca22e7b7c3a0db9d4612e47cbf
nexus0208 <christiandelacruz0208@gmail.com>
2023-05-22 13:49:28 +0800

    edit home names

--- src/Home.jsx
+++ src/Home.jsx
@@ -6,13 +6,9 @@ export default class Home extends Component {
             <div className="home-container" id="home">
                 <div className="max-width">
                     <div className="home-name">
-                        <div className="overflow-hidden">
                             <div className="name">
                                 Christian dela Cruz</div>
-                        </div>
-                        <div className="overflow-hidden">
                         <div className="web-developer">Web Developer</div>
-                    </div>
                 </div>
                     </div>
             </div>

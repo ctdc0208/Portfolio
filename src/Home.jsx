6a9ff1f03abee8372d253e1d2802faea167a878c
nexus0208 <christiandelacruz0208@gmail.com>
2023-05-25 18:17:53 +0800

    fix commit errors

--- src/Home.jsx
+++ src/Home.jsx
@@ -6,34 +6,12 @@ export default class Home extends Component {
             <div className="home-container" id="home">
                 <div className="max-width">
                     <div className="home-name">
-<<<<<<< HEAD
-                        <div className="overflow-hidden">
-                            <div className="first-name">
-                                Christian
-                            </div>
-                        </div>
-                        <div className="overflow-hidden">
-                            <div className="last-name">
-                                dela Cruz
-                            </div>
-                        </div>
-                        <div className="overflow-hidden">
-                            <div className="web">
-                                Web
-                            </div>
-                        </div>
-                        <div className="overflow-hidden">
-                            <div className="developer">Developer</div>
-                        </div>
-                    </div>
-=======
                             <div className="name">
                                 Christian dela Cruz</div>
                         <div className="web-developer">Web Developer</div>
->>>>>>> 6df0afe16d2ec3ab637c061385630f54ce54c546
                 </div>
                     </div>
-
+</div>
         );
     }
 }

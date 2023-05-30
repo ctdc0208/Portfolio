133ee0af884b02be076cd4d25c0416b7fa02e3b0
nexus0208 <christiandelacruz0208@gmail.com>
2023-05-30 17:26:22 +0800

    add about me paragraphs

--- src/About.jsx
+++ src/About.jsx
@@ -3,11 +3,24 @@ import { Component } from "react";
 export default class About extends Component {
     render() {
         return (
-            <div className="max-width about-grid" id="about">
-                <img  src="https://images.pexels.com/photos/4709285/pexels-photo-4709285.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="Profile Photo" className="profile-photo"></img>
+            <div className="max-width about-container" id="about">
                 <div className="profile-bibliography">
-                    <div className="home-about-me margin-15px">About me</div>
-                    <div className="home-bibliography margin-15px">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Accusamus voluptates inventore iste omnis modi, perferendis tempora assumenda, est dignissimos quo nobis at, unde culpa eius beatae eaque molestiae vitae perspiciatis.</div>
+                    <div className="home-about-me">About me</div>
+                    <div className="home-bibliography">
+                        <div>
+                            Hi there! I&apos;m Christian, a web dev.
+                        </div>
+                        <div>
+                            Growing up, I am fascinated by everything tech related, searching the most 
+                            cost-effective device for a specific purpose(smartphones, PC, earphones, etc) 
+                            and building my very first cheap potato PC that always need troubleshooting (LOL), 
+                            are some experiences that help improve my problem-solving skills.
+                        </div>
+                        <div>
+                            Today, web development interest me. Learning new ways, technologies and implementing 
+                            it for eye pleasing designs and making them come to life is what I do!
+                        </div>
+                    </div>
                     <div className="home-links margin-15px">
                         <button className="fa fa-github github-button"></button>
                         <button className="fa fa-linkedin-square linkedin-button" aria-hidden="true"></button>

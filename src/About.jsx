5e59c542ce8d49b568f0ed973b43aeab4f5ee86e
nexus0208 <christiandelacruz0208@gmail.com>
2023-10-16 14:33:25 +0800

    add skills

--- src/About.jsx
+++ src/About.jsx
@@ -24,7 +24,37 @@ export default class About extends Component {
                     <div className="home-links">
                         <a className="fa fa-github github-button" href="https://github.com/nexus0208" target="_blank" rel="noreferrer"></a>
                         <a className="fa fa-linkedin-square linkedin-button" href="https://www.linkedin.com/in/ctdc0208/" target="_blank" rel="noreferrer"></a>
+                    </div>
                 </div>
+                <div className="skills-container">
+                    <div className="dotted-border">
+                        <div className="skills-logo react-logo-svg"></div>
+                        <div className="skills-name">React</div>
+                    </div>
+                    <div className="dotted-border">
+                        <div className="skills-logo node-logo-svg"></div>
+                        <div className="skills-name">Node</div>
+                    </div>
+                    <div className="dotted-border">
+                        <div className="skills-logo html-logo-svg"></div>
+                        <div className="skills-name">HTML</div>
+                    </div>
+                    <div className="dotted-border">
+                        <div className="skills-logo css-logo-svg"></div>
+                        <div className="skills-name">CSS</div>
+                    </div>
+                    <div className="dotted-border">
+                        <div className="skills-logo js-logo-svg"></div>
+                        <div className="skills-name">JavaScript</div>
+                    </div>
+                    <div className="dotted-border">
+                        <div className="skills-logo npm-logo-svg"></div>
+                        <div className="skills-name">npm</div>
+                    </div>
+                    <div className="dotted-border">
+                        <div className="skills-logo cplus-logo-svg"></div>
+                        <div className="skills-name">C++</div>
+                    </div>
                 </div>
             </div>
         );

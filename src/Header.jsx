dd12f7894496da8910a13acbd1d366c668af4763
nexus0208 <christiandelacruz0208@gmail.com>
2023-10-16 17:43:49 +0800

    clean react scroll

--- src/Header.jsx
+++ src/Header.jsx
@@ -9,10 +9,10 @@ export default class Header extends Component {
               <Link activeClass="active" to="home" spy={true} smooth={true} offset={-250} duration={500}>
                 <button className="button-header" >Home</button>
               </Link>
-              <Link activeClass="active" to="about" spy={true} smooth={true} offset={-200} duration={500}>
+              <Link activeClass="active" to="about" spy={true} smooth={true} offset={-125} duration={500}>
                 <button className="button-header">About</button>
               </Link>
-              <Link activeClass="active" to="projects" spy={true} smooth={true} offset={-200} duration={500}>
+              <Link activeClass="active" to="projects" spy={true} smooth={true} offset={-120} duration={500}>
                 <button className="button-header">Projects</button>
               </Link>
               <Link activeClass="active" to="contact" spy={true} smooth={true} offset={-200} duration={500}>

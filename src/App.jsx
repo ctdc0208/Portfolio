529bf8e1f1de1b1d457ff42918c87e44fc9b4af8
nexus0208 <christiandelacruz0208@gmail.com>
2023-10-29 23:32:26 +0800

    add parallax to home

--- src/App.jsx
+++ src/App.jsx
@@ -5,17 +5,18 @@ import About from './About'
 import Projects from './Projects'
 import Contact from './Contact'
 import Footer from './Footer'
+import { ParallaxProvider } from 'react-scroll-parallax'
 
 function App() {
   return (
-    <>
+    <ParallaxProvider>
         <Header />
         <Home />
         <About />
         <Projects />
         <Contact />
         <Footer />
-    </>
+    </ParallaxProvider>
   )
 }
 
--- src/Home.jsx
+++ src/Home.jsx
@@ -1,26 +1,41 @@
 import { Component } from "react";
+import { ParallaxBanner } from "react-scroll-parallax";
 
 export default class Home extends Component {
     render() {
         return (
-            <div className="home-container" id="home">
-                <div className="max-width">
-                    <div className="home-name">
-                        <div className="first-name">
-                            Christian
-                        </div>
-                        <div className="last-name">
-                            dela Cruz
-                        </div>
-                        <div className="web-name">
-                            Web
-                        </div>
-                        <div className="developer-name">
-                            Developer
-                        </div>
-                </div>
-                    </div>
-</div>
+                <ParallaxBanner
+                layers={[
+                    { image: '../src/assets/pexels-daniel-putzer-633409.jpg', speed: -20, 
+                        className: "inset-0 home-background",
+                    },
+                    {
+                        speed: -15,
+                        className: "inset-0",
+                        children: (
+                            <div className="inset-0 max-width">
+                                <div className="home-name">
+                                    <div className="first-name">
+                                    Christian
+                                    </div>
+                                    <div className="last-name">
+                                    dela Cruz
+                                    </div>
+                                    <div className="web-name">
+                                    Web
+                                    </div>
+                                    <div className="developer-name">
+                                    Developer
+                                    </div>
+                                </div>
+                            </div>
+                        ),
+                    }
+                  ]}
+                style={{ aspectRatio: '1.1 / 1' }}
+                id="home"
+                factor={2}
+                />
         );
     }
 }

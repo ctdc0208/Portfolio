63a43da6847e36e677e62cd0c90d0cabcfb5a9ad
Christian dela Cruz <66873179+ctdc0208@users.noreply.github.com>
2024-01-15 12:47:14 +0800

    change config

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,10 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   base: "/Portfolio",
   plugins: [react()],
+
+  define: {
+    // By default, Vite doesn't include shims for NodeJS/
+    // necessary for segment analytics lib to work
+    global: {},
+  },
 })
